4687de1ade36f94c7d59d46e19958569877f27a5
Ntish Choudhary <nitishchoudhary15e@gmail.com>
2024-11-03 23:59:34 +0530

    modified:   app/pages/index.js

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,8 +1,8 @@
 // pages/_app.js
 import '../globals.css'; // Adjusted path based on file location
 
-function MyApp({ Component, pageProps }) {
+function app({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default app;
